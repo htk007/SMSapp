63c41ccef63353ccd3bb3dabc6fcfe944cdf4648
htk007 <hasankaya921@gmail.com>
2018-06-02 16:02:01 +0300

    SMSapp

---
+++ app/src/main/java/com/example/hasan/smsapp/Country.java
@@ -0,0 +1,37 @@
+package com.example.hasan.smsapp;
+
+/**
+ * Created by HASAN on 3.01.2016.
+ */
+public class Country {
+
+    String code = null;
+    String name = null;
+    String continent = null;
+    String region = null;
+
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getContinent() {
+        return continent;
+    }
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+    public String getRegion() {
+        return region;
+    }
+    public void setRegion(String region) {
+        this.region = region;
+    }
+}
